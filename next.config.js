9b55e7aacdea8fca6f4ee4fb0bdd72512ea871db
Curtis Knudson <curtis@midgard.marketing>
2023-01-31 22:19:37 -0700

    Added nostr

--- next.config.js
+++ next.config.js
@@ -1,6 +1,22 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-}
+  async rewrites() {
+    return [
+      // Basic `path-to-regexp` usage
+      // Query object shape: { id: string }
+      { source: "/user/:id", destination: "/user_profile" },
 
-module.exports = nextConfig
+      // Optional Language
+      // Query object shape: { lang?: string }
+      { source: "/:lang(en|es)?/about", destination: "/about" },
+
+      // Advanced rewrite
+      // Query object shape: { id: string } (in addition to dynamic route param)
+      { source: "/u/:id", destination: "/user/:id" },
+      { source: "/api/", destination: "/user/:id" },
+    ];
+  },
+};
+
+module.exports = nextConfig;
